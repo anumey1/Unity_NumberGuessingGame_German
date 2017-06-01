d2d3549747c8059029783ea1e118ebf8afc64312
Anumey Sharma <anumeysharma_shashidhar@srmuniv.edu.in>
2017-06-01 13:29:48 +0530

    Add
    Added unity project.

---
+++ UI Project/Assets/LevelManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+
+    
+    public void LoadLevel(string name)
+    {
+
+        Debug.Log("Level Load requested for: " + name);
+        Application.LoadLevel(name);
+    }
+
+    public void QuitLevel()
+    {
+        
+        Debug.Log("Level quit");
+        Application.Quit();
+
+    }
+
+}
---
+++ UI Project/Assets/NumWiz.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumWiz : MonoBehaviour {
+
+
+    int max;
+    int min;
+    int guess;
+
+    public int mguess = 5;
+
+    public Text text;
+
+    
+
+    // Use this for initialization
+    void Start () {
+        star_game();
+	}
+
+    void star_game()
+    {
+
+        max = 1000;
+        min = 1;
+        next();
+        max = max + 1;
+    }
+
+    public void high()
+    {
+        min = guess;
+        next();
+    }
+
+    public void low()
+    {
+        max = guess;
+        next();
+    }
+
+
+    void next()
+    {
+        guess = Random.Range(min,max+1);
+
+        text.text = guess.ToString();
+
+        mguess = mguess - 1;
+
+
+        if (mguess <= 0)
+        {
+            Application.LoadLevel("WIn");
+
+            
+        }
+
+
+    }
+	
+	// Update is called once per frame
+	
+}
